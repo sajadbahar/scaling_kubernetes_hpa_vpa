2ffc8c314db45ed7028046067dabf0992268d241
Sajad <me@sajadbahar.com>
2024-10-31 12:05:01 +0330

    Use blpop for redis, and flush logs

--- app/worker.py
+++ app/worker.py
@@ -11,18 +11,18 @@ QUEUE_NAME = "notification_queue"
 redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True)
 
 def process_job(job):
-    print(f"Processing job: {job}")
+    print(f"Processing job: {job}", flush=True)
     # Simulate the time to send a push notification
-    time.sleep(1)
+    time.sleep(20)
 
 def main():
-    print("Worker started...")
+    print("Worker started...", flush=True)
     while True:
-        job = redis_client.lpop(QUEUE_NAME)
+        job = redis_client.blpop(QUEUE_NAME, 100)
         if job:
             process_job(job)
         else:
-            print("No jobs in queue. Waiting...")
+            print("No jobs in queue. Waiting...", flush=True)
             time.sleep(5)
 
 if __name__ == "__main__":
